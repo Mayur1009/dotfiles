ae65dc3af8f99f16468134339407e4a8c6bcd075
Mayur1009 <mayur.k.shende@gmail.com>
2024-03-23 10:56:35 +0100

    Changed fd to rg

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -21,7 +21,6 @@ return {
         "stevearc/conform.nvim",
         config = function()
             require("conform").setup({
-
                 notify_on_error = false,
                 format_on_save = {
                     timeout_ms = 500,
