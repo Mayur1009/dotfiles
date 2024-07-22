50275724e1586f2e7f0fc2e72925688012fbb637
Mayur1009 <mayur.k.shende@gmail.com>
2024-07-22 14:04:16 +0200

    t.fish

--- fish/.config/fish/functions/t.fish
+++ fish/.config/fish/functions/t.fish
@@ -1,11 +1,11 @@
 # t
 function t -d "Create or connect to session using fzf"
-    if set -q TMUX
-        echo "Use tmux keybind...(C-a f)"
-        return
-    end
+    # if set -q TMUX
+    #     echo "Use tmux keybind...(C-a f)"
+    #     return
+    # end
 
-    set selected (fzf --tmux --height=50% --walker=dir,follow,hidden --walker-root="$HOME" --walker-skip=Library,.Trash,miniforge3,.cache,.git,node_modules,.venv)
+    set selected (fzf --tmux --height=50% --walker=dir,follow,hidden --walker-root="$HOME" --walker-skip=Library,.Trash,miniforge3,.cache,.git,node_modules,.venv,.nvim_venv,Pictures,Music,Applications,Movies,Public,.vscode,.ssh,.npm,go,.cargo)
 
     if test -z "$selected"
         echo "Nothing selected"
@@ -18,5 +18,9 @@ function t -d "Create or connect to session using fzf"
         tmux new -s "$sname" -n nvim -c "$selected" -d \; send-keys -t "$sname:nvim.1" nvim C-m
     end
 
-    tmux attach -t $sname
+    if set -q TMUX
+        tmux switch -t $sname
+    else
+        tmux attach -t $sname
+    end
 end
