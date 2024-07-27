bf9b81d67e974f258dcd2e7f523b86cc7a955b45
Mayur1009 <mayur.k.shende@gmail.com>
2024-07-27 12:48:05 +0200

    telescope live_grep fix

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,51 +2,19 @@ return {
     { -- Fuzzy Finder (files, lsp, etc)
         "nvim-telescope/telescope.nvim",
         event = "VimEnter",
-        -- branch = "0.1.x",
         dependencies = {
             "nvim-lua/plenary.nvim",
             { -- If encountering errors, see telescope-fzf-native README for install instructions
                 "nvim-telescope/telescope-fzf-native.nvim",
-
-                -- `build` is used to run some command when the plugin is installed/updated.
-                -- This is only run then, not every time Neovim starts up.
                 build = "make",
-
-                -- `cond` is a condition used to determine whether this plugin should be
-                -- installed and loaded.
                 cond = function()
                     return vim.fn.executable("make") == 1
                 end,
             },
             { "nvim-telescope/telescope-ui-select.nvim" },
             { "catgoose/telescope-helpgrep.nvim" },
-
-            -- Useful for getting pretty icons, but requires a Nerd Font.
-            -- { "nvim-tree/nvim-web-devicons",            enabled = vim.g.have_nerd_font },
         },
         config = function()
-            -- Telescope is a fuzzy finder that comes with a lot of different things that
-            -- it can fuzzy find! It's more than just a "file finder", it can search
-            -- many different aspects of Neovim, your workspace, LSP, and more!
-            --
-            -- The easiest way to use telescope, is to start by doing something like:
-            --  :Telescope help_tags
-            --
-            -- After running this command, a window will open up and you're able to
-            -- type in the prompt window. You'll see a list of help_tags options and
-            -- a corresponding preview of the help.
-            --
-            -- Two important keymaps to use while in telescope are:
-            --  - Insert mode: <c-/>
-            --  - Normal mode: ?
-            --
-            -- This opens a window that shows you all of the keymaps for the current
-            -- telescope picker. This is really useful to discover what Telescope can
-            -- do as well as how to actually do it!
-
-            -- [[ Configure Telescope ]]
-            -- See `:help telescope` and `:help telescope.setup()`
-
             local actions = require("telescope.actions")
             local builtin = require("telescope.builtin")
             local all_toggle = false
@@ -59,6 +27,17 @@ return {
                     },
                 },
                 defaults = {
+                    vimgrep_arguments = {
+                        "rg",
+                        "--color=never",
+                        "--no-heading",
+                        "--with-filename",
+                        "--line-number",
+                        "--column",
+                        "--smart-case",
+                        "--hidden",
+                    },
+
                     mappings = {
                         i = {
                             ["<C-Down>"] = actions.cycle_history_next,
@@ -108,8 +87,7 @@ return {
                 builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
                     winblend = 5,
                     previewer = false,
-                })
-                )
+                }))
             end, { desc = "[/] Fuzzily search in current buffer" })
 
             vim.keymap.set("n", "<leader>sc", function()
@@ -117,7 +95,8 @@ return {
             end, { desc = "[S]earch Neovim [c]onfig files" })
 
             require("which-key").add({
-                "<leader>s", group = "+[s]earch",
+                "<leader>s",
+                group = "+[s]earch",
             })
         end,
         keys = {
