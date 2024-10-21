27a969981a3b2b38681fdc203e04e441cffd1584
Mayur1009 <mayur.k.shende@gmail.com>
2024-10-21 21:25:17 +0200

    gen.nvim config

--- nvim/.config/nvim/lua/plugins/gen.lua
+++ nvim/.config/nvim/lua/plugins/gen.lua
@@ -2,39 +2,24 @@ return {
     {
         "David-Kunz/gen.nvim",
         opts = {
-            model = "mistral", -- The default model to use.
-            quit_map = "q", -- set keymap to close the response window
-            retry_map = "<c-r>", -- set keymap to re-send the current prompt
-            accept_map = "<c-cr>", -- set keymap to replace the previous selection with the last result
-            host = "localhost", -- The host running the Ollama service.
-            port = "11434", -- The port on which the Ollama service is listening.
-            display_mode = "float", -- The display mode. Can be "float" or "split" or "horizontal-split".
-            show_prompt = false, -- Shows the prompt submitted to Ollama.
-            show_model = false, -- Displays which model you are using at the beginning of your chat session.
-            no_auto_close = false, -- Never closes the window automatically.
-            file = false, -- Write the payload to a temporary file to keep the command short.
-            hidden = false, -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
-            init = function(options)
-                pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
-            end,
-            -- Function to initialize Ollama
-            command = function(options)
-                local body = { model = options.model, stream = true }
-                return "curl --silent --no-buffer -X POST http://"
-                    .. options.host
-                    .. ":"
-                    .. options.port
-                    .. "/api/chat -d $body"
-            end,
-            -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
-            -- This can also be a command string.
-            -- The executed command must return a JSON object with { response, context }
-            -- (context property is optional).
-            -- list_models = '<omitted lua function>', -- Retrieves a list of model names
-            debug = false, -- Prints errors and the command which is run.
+            model = "codegemma",
+            display_mode = "split",
+            show_prompt = true,
+            show_model = true,
+            no_auto_close = false,
+            file = false,
+            hidden = false,
         },
         config = function(_, opts)
+            require("gen").prompts["Elaborate_Text"] = {
+                prompt = "Elaborate the following text:\n$text",
+            }
+            require("gen").prompts["Comment"] = {
+                prompt = "Write comments for the following code:\n```$filetype\n$text\n```",
+                extract = "```$filetype\n(.-)```",
+            }
             require("gen").setup(opts)
+            vim.keymap.set({ "n", "v" }, "<localleader>g", ":Gen<CR>", { desc = "Gen.nvim Prompt" })
         end,
     },
 }
