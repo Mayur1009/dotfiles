9c218c5eacd497c38c50a3e9d4cb00a91e3b8f02
Mayur1009 <mayur.k.shende@gmail.com>
2024-12-24 17:56:25 +0530

    dashboard change

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -8,8 +8,8 @@ return {
         opts = {
             bigfile = { enabled = true },
             dashboard = {
-                enabled = true,
                 sections = {
+                    { section = "header", gap = 1, padding = 1 },
                     { section = "keys", gap = 1, padding = 1 },
                     {
                         icon = " ",
@@ -19,29 +19,7 @@ return {
                         padding = 1,
                     },
                     { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-                    {
-                        icon = " ",
-                        title = "Git Status",
-                        section = "terminal",
-                        enabled = function()
-                            return Snacks.git.get_root() ~= nil
-                        end,
-                        cmd = "git status --short --branch --renames",
-                        height = 5,
-                        padding = 1,
-                        ttl = 5 * 60,
-                        indent = 3,
-                    },
-                    { section = "header", gap = 1, padding = 1, pane = 2 },
-                    { section = "startup", gap = 1, padding = 1, pane = 2 },
-                    {
-                        section = "terminal",
-                        cmd = "pokemon-colorscripts -r --no-title; sleep .1",
-                        random = 10,
-                        pane = 2,
-                        indent = 16,
-                        height = 20,
-                    },
+                    { section = "startup", gap = 1, padding = 1 },
                 },
             },
             indent = { enabled = true },
