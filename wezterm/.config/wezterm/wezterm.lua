94d2353ac88b32f8845941af74a1cae7ef573119
Mayur1009 <mayur1009@proton.me>
2025-05-14 10:39:03 +0200

    Tmux title show everything

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,9 +7,8 @@ end
 local config = {
     color_scheme = "tokyonight_night",
     enable_tab_bar = false,
-    enable_wayland = false,
     font = wezterm.font("JetBrainsMono Nerd Font Mono"),
-    font_size = 13,
+    -- font_size = 12,
     line_height = 1.10,
     window_padding = {
         left = 2,
