0f1514cedf17b4032910ec25400aabc7fc277e10
Mayur1009 <mayur1009@proton.me>
2025-06-04 21:35:09 +0200

    Cuda injection in python

--- nvim/.config/nvim/queries/python/injections.scm
+++ nvim/.config/nvim/queries/python/injections.scm
@@ -1,7 +1,6 @@
-; extends
+;extends
 
-( (comment) @jps (#match? @jps "^\\# ?\\%\\%") )@code_cell.inner
-
+; CUDA injection
 (
 (comment) @com (#match? @com "# ?\\@cuda")
 (expression_statement
@@ -9,6 +8,7 @@
     left: (identifier)
     right: (string
              (string_start)
-             (string_content) @cuda_string
+             (string_content) @injection.content
              (string_end))))
+    (#set! injection.language "cuda")
 )
